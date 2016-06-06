065618292c11f687ef294e09557141e341ea34e9
Gabriel Aumala <gabrielfima@live.com>
2016-06-06 16:47:55 -0500

    Usar Express en lugar de webpack-dev-server
    se modificaron los scripts para usar express en lugar del dev-server.
    
    npm run build ahora llama a webpack para que genere el archivo public/bundle.js.
    npm start levanta el servidor express que se encuentra en el archivo app.js.
    Esto sirve el index.html de la carpeta public que referencia al bundle.
    
    Los archivos de configuracion del dev-server fueron eliminados. El unico archivo
    de configuracion necesario ahora es webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  devtool: 'eval-source-map',
+  entry:  __dirname + "/src/app/app.js",
+  output: {
+    path: __dirname + "/public",
+    filename: "bundle.js",
+  },
+    module: {
+        loaders: [
+          {
+            test: /\.json$/,
+            loader: "json",
+          },
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+          },
+        ],
+    },
+}
+
+
+
