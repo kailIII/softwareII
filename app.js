1132daec74ad958dacf868476ec3dff613d5a16b
DanielIzquierdo <djizquie@espol.edu.ec>
2016-06-07 03:13:53 -0500

    fetch data from the server to React template

--- app.js
+++ app.js
@@ -1,14 +1,55 @@
-const express = require('express');
+const express     = require('express'),
+      glob            = require('glob'),
+      config          = require('./config/config'),
+      cookieParser    = require('cookie-parser'),
+    bodyParser      = require('body-parser'),
+    session         = require('express-session'),
+      favicon         = require('serve-favicon'),
+      routes = require('./app/controllers/routes.js');
+      fs          = require('fs');
 
-const port = 8080
-const app = express();
+var path = require('path');
+var app = express();
+const port = 8080;
+
+var env = process.env.NODE_ENV || 'development';
+app.locals.ENV = env;
+app.locals.ENV_DEVELOPMENT = env == 'development';
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-      res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+
+// app.get('/', function (req, res) {
+//       res.send('Hello World!');
+// });
+// var controllers = glob.sync(config.root + '/app/controllers/*.js');
+// controllers.forEach(function (controller) {
+//   require(controller)(app);
+// });
+
+
+var models = glob.sync(config.root + '/app/models/*.js');
+models.forEach(function (model) {
+  require(model);
 });
 
+var TYPES_FILE = path.join(__dirname, 'tilesdata.json');
+
+app.post('/api/tipos_habitacion', function (req, res){
+    fs.readFile(TYPES_FILE, function(err, data){
+      if (err){
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(JSON.parse(data));
+    });
+  });
+
 app.listen(port, function () {
-    console.log('Application listening on  port ' + port);
+    console.log('Application listening on  port ' + config.port);
 });
