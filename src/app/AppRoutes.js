298f88c651e6d39034fce5c1eb15baa097ac8cac
dgrockp <dgrockp@github.com>
2016-06-08 20:00:51 -0500

    fixed conflicts

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -10,15 +10,18 @@ import Header from './components/Main';
 import Mostrar_Usuarios from './components/pages/usuarios/mostrar_usuarios';
 import Crear_Usuario from './components/pages/usuarios/crear_usuario';
 import Editar_Usuario from './components/pages/usuarios/editar_usuario';
-import RoomTypesShow from './components/pages/tipos-habitacion/Page'
-import Suites from './components/pages/habitacion/Suites'
-import VerClientes from './components/pages/clientes/verClientes'
+import RoomTypesShow from './components/pages/tipos-habitacion/Page';
+import Suites from './components/pages/habitacion/Suites';
+import VerClientes from './components/pages/clientes/verClientes';
+import Login from './components/login';
+
 
 export default React.createClass ({
     render(){
         return (
             <Router history={hashHistory}>
-              <Route path="/" component={Header} >
+              <Route path="/" component={Login} />
+              <Route path="/header" component={Header} >
                   <Route path="/usuario" component={Mostrar_Usuarios}/>
                   <Route path="/usuario/crear" component={Crear_Usuario}/>
                   <Route path="/usuario/editar" component={Editar_Usuario}/>
