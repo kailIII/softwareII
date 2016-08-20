5ea84d5c1e1be323319d55b37890c55eb0cc5f5a
dgrockp <dgrockp@gmail.com>
2016-08-20 15:01:49 -0500

    eliminado archivo update_cliente, ahora se usa un solo componente para agregar o editar un cliente

--- src/app/data/clients.js
+++ src/app/data/clients.js
@@ -1,9 +1,7 @@
-const clients = [
+module.exports = [
     {client_id: 1, nombre:"Juan Díaz", cedula:"09923398713", nacionalidad:"Ecuatoriana", telefono:"09923453627", email:"client@gmail.com"},
     {client_id: 2, nombre:"Carlos Sánchez", cedula:"09923398423", nacionalidad:"Ecuatoriana", telefono:"09923453627", email:"client@gmail.com"},
     {client_id: 3, nombre:"Pedro Ortega", cedula:"09923354713", nacionalidad:"Ecuatoriana", telefono:"09923453627", email:"client@gmail.com"},
     {client_id: 4, nombre:"Jorge Pérez", cedula:"09922344713", nacionalidad:"Ecuatoriana", telefono:"09923453627", email:"client@gmail.com"},
     {client_id: 5, nombre:"Esteban Páez", cedula:"09922344721", nacionalidad:"Ecuatoriana", telefono:"09923453627", email:"client@gmail.com"},
 ]
-
-export default clients
